47e01dc41782a00244dbbd22a6b766e591fcb17b
Cristina Leandro -  Shift (Desenvolvimento) <cristina.leandro@shift.com.br>
2024-04-30 08:58:47 -0300

    guessing game

---
+++ lib/src/guessingGame-Day-02/guess.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class guess {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+
+        int counter = 1;
+        int number = random.nextInt(10);
+
+        System.out.println("Guess a number between 0 and 10: ");
+        int guess = scan.nextInt();
+
+        while (guess != number) {
+            System.out.println("Wrong answer :(  ");
+            if (guess > number) {
+                System.out.println("Guess lower:");
+            } else if (guess < number) {
+                System.out.println("Guess higher:");
+            }
+            guess = scan.nextInt();
+            counter++;
+        }
+
+        if (number == guess) {
+            System.out.println("Correct! The number was " + number + ".");
+            System.out.println("You tried to guess the number " + counter + " times.");
+        }
+    }
+}

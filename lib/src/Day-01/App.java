0605b045c499d4225b3077e8b6a33545053a5a42
Cristina Leandro -  Shift (Desenvolvimento) <cristina.leandro@shift.com.br>
2024-05-01 21:46:47 -0300

    to do list

--- lib/src/Day-01/App.java
+++ lib/src/Day-01/App.java
@@ -58,7 +58,6 @@ public class App {
                     System.out.println("invalid number, girly. try again;");
 
                     System.out.println("\n\n");
-
             }
         }
     }

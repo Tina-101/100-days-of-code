5bfe04383e237dfba51b0812a00ecbf011fcd34e
Cristina Leandro -  Shift (Desenvolvimento) <cristina.leandro@shift.com.br>
2024-05-01 21:42:31 -0300

    personal diary

--- lib/src/Day-03/Diary.java
+++ lib/src/Day-03/Diary.java
@@ -1,3 +1,54 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Diary {
 
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<Entries> entryList = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+
+        String menu = "shall we journal today? type: \n1 to add a new entry \n2 to list all entries \n3 to exit. \n\n";
+        System.out.println(menu);
+
+        while (true) {
+            int choice = scan.nextInt();
+            scan.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("today's date: ");
+                    String todaysDate = scan.nextLine();
+
+                    System.out.println("today's entry: ");
+                    String todaysEntry = scan.nextLine();
+
+                    Entries entry = new Entries(todaysDate, todaysEntry);
+                    entryList.add(entry);
+
+                    System.out.println("Entry added. \n\n");
+                    break;
+
+                case 2:
+                    System.out.println("Entries: ");
+                    for (int i = 0; i < entryList.size(); i++) {
+                        Entries e = entryList.get(i);
+                        System.out.println((i + 1) + "\n" + e.getEntry() + "\n" + e.getDate() + " \n");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Bye! ");
+                    scan.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("invalid input. \nPress 1 to add a new entry or 3 to exit.");
+                    choice = scan.nextInt();
+                    break;
+            }
+        }
+
+    }
 }
--- lib/src/Day-03/Entries.java
+++ lib/src/Day-03/Entries.java
@@ -1,3 +1,26 @@
 public class Entries {
 
+    private String entry;
+    private String date;
+
+    public Entries(String entry, String date) {
+        this.entry = entry;
+        this.date = date;
+    }
+
+    public String getEntry() {
+        return entry;
+    }
+
+    public void setEntry() {
+        this.entry = entry;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate() {
+        this.date = date;
+    }
 }
